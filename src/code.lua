4a4b45917a294400bcbc639f6ed0e30953582dad
khuxkm fbexl <khuxkm@ttm.sh>
2019-07-17 13:26:03 -0400

    Test BF interpreter

--- src/code.lua
+++ src/code.lua
@@ -1,12 +1,20 @@
 local code = {}
 
-function code.init() end
+local bf = require "bf"
 
-function code.update() end
+bf.program = "+[+]"
+
+function code.init()
+	love.graphics.setFont(love.graphics.newFont(24))
+end
+
+function code.update()
+	bf.runInstruction()
+end
 
 function code.draw()
 	pal.setBackgroundColor(0)
-	pal.setColor(15)
+	pal.setColor(bf.memory[1]%16)
 	love.graphics.print("Hello world!",1,1)
 end
 
